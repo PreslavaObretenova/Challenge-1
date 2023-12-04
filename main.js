06fdb4eeab47e058373e96e76efa53046b0d7026
PreslavaObretenova <st175849@stud.uni-stuttgart.de>
2023-12-04 13:55:10 +0100

    Initial commit

--- main.js
+++ main.js
@@ -2,13 +2,19 @@
 import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import GUI from 'lil-gui';
+import GUI, { ColorController } from 'lil-gui';
 
 //CONSTANT & VARIABLES
 let width = window.innerWidth;
 let height = window.innerHeight;
 //-- GUI PAREMETERS
- 
+ var gui;
+ const parameters = {
+  resolutionX: 3,
+  rotationX: 100
+ }
+
+
 //-- SCENE VARIABLES
 var gui;
 var scene;
@@ -21,16 +27,23 @@ var directionalLight;
 
 //-- GEOMETRY PARAMETERS
 //Create an empty array for storing all the cubes
- 
+let sceneCubes = []; 
+ let resX = parameters.resolutionX;
+ let rotX = parameters.rotationX;
+
 
 function main(){
   //GUI
-    
+    gui = new GUI;
+    gui.add(parameters, 'resolutionX', 1, 10, 1);
+    gui.add(parameters, 'rotationX', 0, 180);
+
 
   //CREATE SCENE AND CAMERA
   scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera( 35, width / height, 0.1, 100);
+  camera = new THREE.PerspectiveCamera( 55, width / height, 0.1, 100);
   camera.position.set(10, 10, 10)
+
 
   //LIGHTINGS
   ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
@@ -44,6 +57,8 @@ function main(){
 
   //GEOMETRY INITIATION
   // Initiate first cubes
+  createCubes();
+  rotateCubes();
 
 
   //RESPONSIVE WINDOW
@@ -66,6 +81,72 @@ function main(){
 //-----------------------------------------------------------------------------------
 //HELPER FUNCTIONS
 //-----------------------------------------------------------------------------------
+
+
+
+//GEOMETRY FUNCTIONS 
+// Create Cubes
+function createCubes(){
+ 
+  for(let i=0; i<resX; i++){
+   const geometry = new THREE.CylinderGeometry(0.4, 5,1);
+   const material = new THREE.MeshPhysicalMaterial();
+   material.color =  new THREE.Color(0x90ee90);
+   material.color.setRGB(0,1,Math.random(0x90ee90));
+   
+   const cube = new THREE.Mesh(geometry, material);
+   cube.position.set(i*0.1, 0, 0);
+   cube.name = "cube " + i;
+   sceneCubes.push(cube);
+
+   scene.add(cube);
+  }
+}
+
+//Rotate Cubes
+function rotateCubes(){
+ sceneCubes.forEach((element, index)=>{
+  let scene_cube = scene.getObjectByName(element.name);
+  let radian_rot = (index*(rotX/resX)) * (Math.PI/180);
+  scene_cube.rotation.set(radian_rot, 0, 0)
+  rotX = parameters.rotationX;
+ }) 
+}
+
+//Remove 3D Objects and clean the caches
+function removeObject(sceneObject){
+   if (!(sceneObject instanceof THREE.Object3D)) return;
+
+   //Remove the geometry to free GPU resources
+   if (sceneObject.geometry) sceneObject.geometry.dispose();
+
+   //Remove the materials to free the GPU resources
+   if(sceneObject.material){
+    if (sceneObject.material instanceof Array) {
+      sceneObject.material.forEach(material => material.dispose());
+    } else {
+      sceneObject.material.dispose();
+    }
+   }
+   
+   //Remove object from scene
+   sceneObject.removeFromParent();
+}
+
+//Remove the cubes
+function removeCubes(){
+  resX = parameters.resolutionX; 
+  rotX = parameters.rotationX;
+
+  sceneCubes.forEach(element =>{
+    let scene_cube = scene.getObjectByName(element.name);
+    removeObject(scene_cube);
+  })
+
+  sceneCubes = [];
+}
+
+
 //RESPONSIVE
 function handleResize() {
   width = window.innerWidth;
@@ -76,17 +157,33 @@ function handleResize() {
   renderer.render(scene, camera);
 }
 
+//RANDOM INTEGER IN A RANGE
+function getRndInteger(min, max){
+  return Math.floor(Math.random() * (max-min+1)) + min;
+}
+
 //ANIMATE AND RENDER
 function animate() {
 	requestAnimationFrame( animate );
  
+  control.update();
+
+  if(resX != parameters.resolutionX){
+    removeCubes();
+    createCubes();
+    rotateCubes();
+  }
  
+  if (rotX != parameters.rotationX){
+    rotateCubes();
+  }
+
 	renderer.render( scene, camera );
 }
 //-----------------------------------------------------------------------------------
 // CLASS
 //-----------------------------------------------------------------------------------
-
+ 
 
 //-----------------------------------------------------------------------------------
 // EXECUTE MAIN 
